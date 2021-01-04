d16bab90227b08a24efa3ec02c2273e7e29cdee3
Raul Barrera <rbarrera1992@gmail.com>
2021-01-04 16:19:32 -0600

    fixed main within main

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -10,14 +10,14 @@ export default class Result extends Component {
           ? <h2>You were correct! :D</h2>
           : <h2>Good try, but not quite right :(</h2>
         }
-        <main className='DisplayScore'>
+        <div className='DisplayScore'>
           <p>Your total score is: {data.totalScore}</p>
-        </main> 
-        <main className='DisplayFeedback'>
+        </div> 
+        <div className='DisplayFeedback'>
           <p>
             The correct translation for {word} was {data.answer} and you chose {guess.toLowerCase()}!
           </p>
-        </main>
+        </div>
       </>
     )
   }
